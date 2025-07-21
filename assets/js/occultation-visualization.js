71d8b4b93752be56f1cb1ac78bce0b6cd3189c01
Mapoet <Mapoet.Niphy@Gmail.com>
2025-07-21 14:05:20 +0800

    fix: enable keyboard input handling in Cesium Viewer for improved user interaction and control

--- assets/js/occultation-visualization.js
+++ assets/js/occultation-visualization.js
@@ -76,7 +76,7 @@ function initCesiumViewer() {
         shouldAnimate: true, // 动画
         requestRenderMode: false, // 关闭请求渲染模式
         targetFrameRate: 60, // 目标帧率
-        keyboardInputEvent: false // 禁用Cesium的键盘输入处理，使用我们自己的
+        keyboardInputEvent: true // 启用Cesium的键盘输入处理，使用我们自己的
     });
     
     // 配置时间轴样式
